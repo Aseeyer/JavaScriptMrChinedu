ff1051324859cc30379de9366b3b7d89d04fc710
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-07-28 00:34:20 +0100

    Added basic required functions without their logic

---
+++ PasswordStrength.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+function getPassword(){}
+function checkComplexity(password){}
+function evaluateStrength(alphaCount, numb_count){}
+main()
