748a9943428ac1d5302f93ebbe8a9d70758949cf
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-08-11 21:16:23 +0100

    Added task 24

---
+++ 50FunChallenges/NestedEvenChecker.js
@@ -0,0 +1,9 @@
+function nestedEvenChecker() {
+    for (let nest = 1; nest <= 10; nest++) {
+        if (nest % 2 === 0) {
+            console.log(nest);
+        }
+    }
+}
+
+nestedEvenChecker();
