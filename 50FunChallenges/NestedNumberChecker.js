21dee76b3b0e8c300c444814d76980ae1b26a314
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-08-11 23:07:11 +0100

    Added task 41

---
+++ 50FunChallenges/NestedNumberChecker.js
@@ -0,0 +1,17 @@
+function nestedNumberChecker() {
+    let number = 1;
+    while (number <= 15) {
+        if (number % 3 === 0 && number % 5 === 0) {
+            console.log("FizzBuzz!");
+        } else if (number % 3 === 0) {
+            console.log("Fizz!");
+        } else if (number % 5 === 0) {
+            console.log("Buzz!");
+        } else {
+            console.log(number);
+        }
+        number++;
+    }
+}
+
+nestedNumberChecker();
